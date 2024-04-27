7902b27a1b64352a515bf31351ddbb53849851f8
Dev Patel <dpatel222430@gmail.com>
2024-04-28 02:39:35 +0530

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Dev Patel on 28/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
